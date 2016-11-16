f5e7821addc27e02b041d5cbaebe7057fb0eb5a8
Michael Roque <mikeroque1@gmail.com>
2016-11-16 09:31:16 -0700

    Got click event to load next recipe

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-// $.get('https://www.spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random', recipeImage)
-// $.get('response.json', recipeImage)
-$.ajax({
-  url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false',
-  headers: {
-    'X-Mashape-Key': 'GOGW0xr8qymshxN4nRHm0XFgvOfJp1zMGFujsnMBwfLBfmA1pY',
-    Accept: 'application/json'
-  },
-  success: recipeImage,
-  type: 'GET',
-})
+function getRandomRecipe() {
+    $.ajax({
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false',
+        headers: {
+            'X-Mashape-Key': 'GOGW0xr8qymshxN4nRHm0XFgvOfJp1zMGFujsnMBwfLBfmA1pY',
+            Accept: 'application/json'
+        },
+        success: recipeImage,
+        type: 'GET',
+    })
 
-function recipeImage(data) {
-  console.log(data);
-    var recipeName = data.recipes[0].title
-    var ingredients = data.recipes[0].extendedIngredients[0]
-    var image = data.recipes[0].image
-    var instructions = data.recipes[0].instructions
-    var link = data.recipes[0].sourceUrl
-    console.log(recipeName);
-    console.log(link);
-    console.log(image);
-    console.log(instructions);
+    function recipeImage(data) {
+        var recipeName = data.recipes[0].title
+        var ingredients = data.recipes[0].extendedIngredients[0]
+        var image = data.recipes[0].image
+        var instructions = data.recipes[0].instructions
+        var link = data.recipes[0].sourceUrl
+        var servings = data.recipes[0].servings
+        var readyIn = data.recipes[0].readyInMinutes
+
+        $('#recipeImage').attr('src', image);
+        $('#recipeName').attr('href', link);
+        $('#recipeName').text(recipeName)
+        $('#instructions').text(instructions)
+        $('#servings').text(servings + ' servings')
+        $('#ready').text('Ready in ' + readyIn + ' minutes')
+    }
 
 }
+
+$('.arrows').click(function() {
+  getRandomRecipe()
+});
+
+getRandomRecipe()
